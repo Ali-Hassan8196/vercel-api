e45c35b59ec82169a620d2a4a7507100d02ef70d
Ali-Hassan8196 <alihassan30714@gmail.com>
2023-07-25 12:45:29 +0500

    updated vercel file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const app = express();
+const port = 3000;
 
-app.get('/api',(req,res)=>{
+app.use('/',(req,res)=>{
     res.send('Hello vercel!')
 })
  app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${port}`);
  })
